48ae9607d3e9023ed17c8b25f3127433aeb2b796
Liu Ji <jimnox0491@gmail.com>
2015-11-08 01:40:30 +0800

    optimize player ui

--- WebApp/src/components/control-panel.vue
+++ WebApp/src/components/control-panel.vue
@@ -17,6 +17,12 @@
   align-items: center;
   flex: 1;
 
+  .range {
+    margin-left: 8px;
+    font-size: 14px;
+    align-items: flex-end;
+  }
+
   .control {
     justify-content: flex-end;
     padding-right: 4px;
@@ -50,12 +56,16 @@
 <template>
   <div class="control-panel h-box">
     <div class="player h-box">
+      <div class="range v-box">
+        <span class="time">{{rangeStart}}</span>
+        <span class="time">{{rangeEnd}}</span>
+      </div>
       <div class="progress">
         <progress-bar :progress="playProgress"></progress-bar>
       </div>
       <div class="control h-box">
-        <span class="time h-box">片段时长：{{duration}}</span>
-        <i class="play fa fa-play" :class="[(playing ? 'fa-stop' : 'fa-play')]" @click="play"></i>
+        <span class="time h-box">{{played}} / {{duration}}</span>
+        <i class="play fa fa-play" :class="[(playing ? 'fa-stop' : 'fa-play')]" @click="togglePlay"></i>
       </div>
     </div>
 
@@ -82,43 +92,68 @@ module.exports = {
     disable() {
       return !(this.ad && this.file)
     },
+    rangeStart() {
+      if (!this.ad) return '--:--'
+      return _.formatDuration(this.ad.start, false)
+    },
+    rangeEnd() {
+      if (!this.ad) return '--:--'
+      return _.formatDuration(this.ad.end, false)
+    },
+    played() {
+      if (!this.ad) return '--:--'
+      if (!this.playing) return '0:00'
+      var time = this.playTime - this.ad.start
+      return _.formatDuration(time, false)
+    },
     duration() {
-      if (!this.ad) return '--:---'
+      if (!this.ad) return '--:--'
       var time = this.ad.end - this.ad.start
       return _.formatDuration(time, false)
     },
     tagButtonText() {
-      if (!this.ad) return ''
-      return this.ad.ignored ? '这是广告' : '不是广告'
+      return (this.ad && this.ad.ignored) ? '确是广告' : '并非广告'
     },
     playProgress() {
       if (!this.ad || !this.file) return 0
       if (!this.playing) return 0
       if (this.playTime <= 0) return 0
-      return this.playTime / this.file.length
+      //return this.playTime / this.file.length // 绝对进度
+      return (this.playTime - this.ad.start) / (this.ad.end - this.ad.start) // 相对进度
     }
   },
+  created() {
+    this.$watch('ad', function() {
+      this.stop()
+    })
+  },
   methods: {
+    stop() {
+      clearInterval(this.playTimeer)
+      this.playing = false
+      this.playTime = 0
+      bound.stop()
+    },
     play() {
-      if (!this.playing) {
-        this.playing = true
-        var freq = 200
-        var beginTime = _.timestamp()
-        this.playTime = this.ad.start
-        this.playingTimer = setInterval(() => {
-          this.playTime = this.ad.start + _.timestamp() - beginTime
-        }, freq)
+      this.playing = true
+      var freq = 100
+      var beginTime = _.timestamp()
+      this.playTime = this.ad.start
+      this.playTimeer = setInterval(() => {
+        this.playTime = this.ad.start + _.timestamp() - beginTime
+      }, freq)
 
-        bound.play(this.file.fullname, this.ad.start, this.ad.end, () => {
-          clearInterval(this.playTimeer)
-          this.playing = false
-          this.playTime = 0
-        })
-      } else {
+      bound.play(this.file.fullname, this.ad.start, this.ad.end, () => {
         clearInterval(this.playTimeer)
         this.playing = false
         this.playTime = 0
-        bound.stop()
+      })
+    },
+    togglePlay() {
+      if (!this.playing) {
+        this.play()
+      } else {
+        this.stop()
       }
     },
     tagAd() {
