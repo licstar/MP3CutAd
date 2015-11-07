a4ebabbd1d882694be9b2179afd0a923f300eb34
licstar <licstar@sina.com>
2015-11-07 14:32:41 +0800

    各种重构

---
+++ MP3CutAd/FFMpeg.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP3CutAd {
+    class FFMpeg {
+        private static string path = @"D:\tools\ffmpeg-20151101-git-dee7440-win64-static\bin\ffmpeg";
+
+        private static void run(string argument) {
+            Process p = new Process();
+            p.StartInfo.FileName = path;
+            p.StartInfo.Arguments = argument;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.Start();
+
+            p.StandardError.ReadToEnd();
+            p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+        }
+
+        public static void Mp3toWav(string mp3, string wav) {
+            run(string.Format("-i \"{0}\" -ac 1 -ar 22050 -f wav \"{1}\"", mp3, wav));
+        }
+
+        public static void Split(string mp3, string output, double from, double to) {
+            run(string.Format("-i \"{0}\" -c copy -ss {2} -to {3} \"{1}\"",
+                mp3, output, from, to));
+        }
+
+        public static void Concat(string list, string output) {
+            run(string.Format("-f concat -i \"{0}\" -c copy \"{1}\"", list, output));
+        }
+    }
+}
---
+++ MP3CutAd/LSH.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP3CutAd {
+    class LSH {
+        public static double sim(double[,] a, double[,] b, int offsetA, int offsetB, int size) {
+            double f = 0, na = 0, nb = 0;
+
+            for (int i = offsetA, j = offsetB; i < offsetA + size; i++, j++) {
+                for (int k = 0; k < a.GetLength(1); k++) {
+                    f += a[i, k] * b[j, k];
+                    na += a[i, k] * a[i, k];
+                    nb += b[j, k] * b[j, k];
+                }
+            }
+            return f / Math.Sqrt(na * nb + 1e-8);
+        }
+
+        static List<double[,]> vectors = null;
+
+        public static void init(int s1, int s2) { //初始化随机向量
+            Random r = new Random(1);
+            int n = 12;
+            vectors = new List<double[,]>();
+            for (int id = 0; id < n; id++) {
+                var vector = new double[s1, s2];
+                for (int i = 0; i < s1; i++) {
+                    for (int j = 0; j < s2; j++) {
+                        vector[i, j] = r.NextDouble() - 0.5;
+                    }
+                }
+                vectors.Add(vector);
+            }
+        }
+
+        public static int hash(double[,] a, int offsetA) {
+            int ret = 0;
+            for (int i = 0; i < vectors.Count; i++) {
+                double v = sim(a, vectors[i], offsetA, 0, vectors[i].GetLength(0));
+                ret <<= 1;
+                if (v <= 0) {
+                    ret |= 1;
+                }
+            }
+            return ret;
+        }
+    }
+}
